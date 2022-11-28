4ba2826a32c5eac36f8dd537632640069c27558b
ryry106 <ryrymrs9@gmail.com>
2022-11-28 20:32:10 +0900

    update SPDaily

--- tasklist.go
+++ tasklist.go
@@ -15,9 +15,16 @@ type Task struct {
 	FixedDt  time.Time
 }
 
+type SPDailyList struct {
+	List []SPDaily
+}
+type SPDaily struct {
+	Dt time.Time
+	SP int
+}
+
 func (tl *TaskList) SPDaily() {
 
-
-  fmt.Println("test")
+	fmt.Println("test")
 
 }
