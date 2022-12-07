7cfb5bbda64b42d62243f4ea04963887e67a7811
ryry106 <ryrymrs9@gmail.com>
2022-12-07 22:53:09 +0900

    update

--- facerd/preview.go
+++ facerd/preview.go
@@ -1,64 +1,14 @@
 package facerd
 
 import (
-	"fmt"
-	"io"
-	"net/http"
-	"os"
-	"ryry/prev-bdc/model"
-	"time"
-
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+	"ryry/prev-bdc/preview"
 )
 
 func Run(csvPath string) {
-	// todo file存在チェック
+  // todo ファイルのチェックなど
 
-	// Echo instance
-	e := echo.New()
 
-	// Middleware
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
 
-	r := &Resource{csvPath: csvPath}
-
-	// Routes
-	e.GET("/preview", preview)
-	e.GET("/resource", r.resource)
-
-	// Start server
-	e.Logger.Fatal(e.Start(":8080"))
-}
-
-func preview(c echo.Context) error {
-	f, err := os.Open("facerd/template.html")
-	if err != nil {
-		return c.String(http.StatusInternalServerError, "")
-	}
-	template, err := io.ReadAll(f)
-	if err != nil {
-		return c.String(http.StatusInternalServerError, "")
-	}
-	c.Response().Header().Set(echo.HeaderContentType, "text/html; charset=UTF-8")
-	return c.String(http.StatusOK, string(template))
-}
-
-type Resource struct {
-	csvPath string
-}
-
-// Handler
-func (r *Resource) resource(c echo.Context) error {
-	tl, err := model.ToTaskList(r.csvPath)
-	if err != nil {
-		return c.String(http.StatusInternalServerError, "")
-	}
-	fmt.Println(tl)
-	tz, err := time.LoadLocation("Asia/Tokyo")
-	if err != nil {
-		return c.String(http.StatusInternalServerError, "")
-	}
-	return c.String(http.StatusOK, tl.ToSPDaily(time.Now().In(tz)).ToJson())
+	serv := &preview.Serv{CsvPath: csvPath}
+	serv.Up()
 }
---
+++ preview/serv.go
@@ -0,0 +1,71 @@
+package preview
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"ryry/prev-bdc/model"
+	"time"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+type Serv struct {
+	CsvPath string
+}
+
+func (s *Serv) Up() {
+	// Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	ph := &PrevHandler{templateHtmlPath: "facerd/template.html"}
+	rh := &ResourceHandler{csvPath: s.CsvPath}
+
+	// Routes
+	e.GET("/preview", ph.handle)
+	e.GET("/resource", rh.handle)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":8080"))
+
+}
+
+type PrevHandler struct {
+	templateHtmlPath string
+}
+
+func (ph *PrevHandler) handle(c echo.Context) error {
+	f, err := os.Open(ph.templateHtmlPath)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "")
+	}
+	template, err := io.ReadAll(f)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "")
+	}
+	c.Response().Header().Set(echo.HeaderContentType, "text/html; charset=UTF-8")
+	return c.String(http.StatusOK, string(template))
+}
+
+type ResourceHandler struct {
+	csvPath string
+}
+
+func (r *ResourceHandler) handle(c echo.Context) error {
+	tl, err := model.ToTaskList(r.csvPath)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "")
+	}
+	fmt.Println(tl)
+	tz, err := time.LoadLocation("Asia/Tokyo")
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "")
+	}
+	return c.String(http.StatusOK, tl.ToSPDaily(time.Now().In(tz)).ToJson())
+}
