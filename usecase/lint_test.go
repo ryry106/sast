fefea88a4b9e7b9d2438361cc9b0451d46d27baa
ryry106 <ryrymrs9@gmail.com>
2022-12-31 21:30:19 +0900

    clean test

--- usecase/lint_test.go
+++ usecase/lint_test.go
@@ -3,6 +3,7 @@ package usecase
 import (
 	"bytes"
 	"path/filepath"
+	"strings"
 	"testing"
 )
 
@@ -11,12 +12,10 @@ func TestDo(t *testing.T) {
 	NewLint(filepath.Dir("tests/lint")).Do(b)
 
 	actual := b.String()
-	expects := `tests/lint/2.csv
-    ,2022-12-05,2022-12-06 | strconv.Atoi: parsing "": invalid syntax
-tests/lint/1.csv
-    1,,2022-12-03 | parsing time "" as "2006-01-02": cannot parse "" as "2006"
-`
-	if expects != actual {
-		t.Errorf("fail. expects %s, actual %s", expects, actual)
+	expects := []string{"tests/lint/2.csv", ",2022-12-05,2022-12-06 | strconv.Atoi: parsing \"\": invalid syntax", "tests/lint/1.csv", "1,,2022-12-03 | parsing time \"\" as \"2006-01-02\": cannot parse \"\" as \"2006\""}
+	for _, e := range expects {
+		if strings.Index(actual, e) == -1 {
+			t.Errorf("fail. display %s", actual)
+		}
 	}
 }
